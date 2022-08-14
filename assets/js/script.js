0620331f6117665778f334de70aebcdac4eb4ce3
CHRIS <chris@CHRISs-MBP.attlocal.net>
2022-08-13 21:21:47 -0700

    built exercise function

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+document.write('Hello!');
+
+//exercise button functionality (need button name to call function)
+var exerciseBtn = document.getElementById('excerBtn')
+
+exerciseBtn.addEventListener('click',function() {
+    var input = document.createElement("input");
+    input.type = "search";
+    input.id = "searchBar";
+    var searchBtn = document.createElement("button");
+    btn.innerHTML = "Submit";
+    btn.type = "submit";
+    btn.name = "searchBtn";
+    document.body.appendChild(input);
+});
+
+searchBtn.addEventListener('click', function() {
+    var exerciseType = document.getElementById('searchBar').value;
+    if (exerciseType = "") {
+
+        $.ajax({
+            method: 'GET',
+            url: 'https://api.api-ninjas.com/v1/exercises?name=' + exerciseType,
+            headers: { 'X-Api-Key': 'GKg0l9hlc0fRJEHUdIsVzw==lti9bU3OVAYwF8Wk'},
+            contentType: 'application/json',
+            success: function(result) {
+                console.log(result);
+            },
+            error: function ajaxError(jqXHR) {
+                console.error('Error: ', jqXHR.responseText);
+            }
+        });
+
+    }  
+});
+
