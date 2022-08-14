6d918a7bce77172198e827de85a0e39438cab1de
CHRIS <chris@CHRISs-MBP.attlocal.net>
2022-08-14 14:40:29 -0700

    update javascript and add html elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,50 @@
 document.write('Hello!');
 
 //exercise button functionality (need button name to call function)
-var exerciseBtn = document.getElementById('excerBtn')
+// var exerciseBtn = document.getElementById('excerBtn')
 
-exerciseBtn.addEventListener('click',function() {
-    var input = document.createElement("input");
-    input.type = "search";
-    input.id = "searchBar";
-    var searchBtn = document.createElement("button");
-    btn.innerHTML = "Submit";
-    btn.type = "submit";
-    btn.name = "searchBtn";
-    document.body.appendChild(input);
+// exerciseBtn.addEventListener('click',function() {
+//     var input = document.createElement("input");
+//     input.type = "search";
+//     input.id = "searchBar";
+
+//     var searchBtn = document.createElement("button");
+//     btn.innerHTML = "Submit";
+//     btn.type = "submit";
+//     btn.name = "searchBtn";
+//     document.body.appendChild(input);
+// });
+$(function(){
+    $(".exercise").hide();
 });
 
-searchBtn.addEventListener('click', function() {
-    var exerciseType = document.getElementById('searchBar').value;
-    if (exerciseType = "") {
+$("#exerBtn").click(function() {
+    console.log("exercise"); // or alert($(this).attr('id'));
+    $('.nutrition').hide();
 
-        $.ajax({
-            method: 'GET',
-            url: 'https://api.api-ninjas.com/v1/exercises?name=' + exerciseType,
-            headers: { 'X-Api-Key': 'GKg0l9hlc0fRJEHUdIsVzw==lti9bU3OVAYwF8Wk'},
-            contentType: 'application/json',
-            success: function(result) {
-                console.log(result);
-            },
-            error: function ajaxError(jqXHR) {
-                console.error('Error: ', jqXHR.responseText);
-            }
-        });
+    $('.exercise').show();
 
-    }  
+
 });
+
+
+// searchBtn.addEventListener('click', function() {
+//     var exerciseType = document.getElementById('searchBar').value;
+//     if (exerciseType = "") {
+
+//         $.ajax({
+//             method: 'GET',
+//             url: 'https://api.api-ninjas.com/v1/exercises?name=' + exerciseType,
+//             headers: { 'X-Api-Key': 'GKg0l9hlc0fRJEHUdIsVzw==lti9bU3OVAYwF8Wk'},
+//             contentType: 'application/json',
+//             success: function(result) {
+//                 console.log(result);
+//             },
+//             error: function ajaxError(jqXHR) {
+//                 console.error('Error: ', jqXHR.responseText);
+//             }
+//         });
+
+//     }  
+// });
 
