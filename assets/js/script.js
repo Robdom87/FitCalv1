b7d711cad955a8389e885254df64dd2e0756fab3
Cedar Chavez <geggas00@gmail.com>
2022-08-15 19:21:24 -0700

    add nutrition info print function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,49 +4,49 @@ let food = "";
 let amountInput = $('#amountInput');
 let unitInput = $('#unitInput');
 let itemInput = $('#itemInput');
+let table= $('table');
 
-$('#submitBtn').on('click',function(){
+$('#submitBtn').on('click', function () {
     //assign user input to API queries and clean up extra spaces
-    amount = parseInt(amountInput.val().trim(),10);
+    amount = parseInt(amountInput.val().trim(), 10);
 
-    if (isNaN(amount)) {
-        //add modal to tell user that only numbers.
-        return;
-    }
-    
-    if (amount < 0) {
-        //add modal to tell user that only positive whole numbers.
-        return;
-    }
-    
     //round as only whole numbers are taken
     amount = Math.round(amount).toString();
-    console.log(amount);
+
+    food = itemInput.val();
+    food.trim().toLowerCase();
+    food.split("");
+
+    //remove any spaces within the item and replace with %20
+    for (let i = 0; i < food.length; i++) {
+        if (food[i] = " ") {
+            food[i] = "%20";
+        }
+    }
+    food.toString();
 
     unit = unitInput.val();
-    console.log(unit);
-    
-    food = itemInput.val();
 
-    // if (false) { //add criteria to check if all string is text
-    //     //add modal to tell user that food item contains special characters
-    //     return;
-    // }
+    //if unit input is used, use fetch request with only food item name
+    if (unit === "item") {
+        let itemUrl = 'https://api.api-ninjas.com/v1/nutrition?query=' + food;
+        fetch(itemUrl, {
+            headers: {
+                'X-Api-Key': 'GKg0l9hlc0fRJEHUdIsVzw==lti9bU3OVAYwF8Wk'
+            }
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                printNutrition(data);
+            });
+            return;
+    }
 
-    // food = fd.trim(fd).toLowerCase();
-    // food.split("");
-
-    // //remove any spaces within the item and replace with %20
-    // for (let i = 0; i < food.length; i++) {
-    //     if (food[i]=" ") {
-    //         food[i]="%20";
-    //     }
-    //   }
-    // food = food.toString();
-    //call API with user input
-    console.log(food);
-
-    let requestUrl = 'https://api.api-ninjas.com/v1/nutrition?query='+amount+'%20'+unit+'%20'+food;
+    // call API with user input
+    let requestUrl = 'https://api.api-ninjas.com/v1/nutrition?query=' + amount + '%20' + unit + '%20' + food;
     fetch(requestUrl, {
         headers: {
             'X-Api-Key': 'GKg0l9hlc0fRJEHUdIsVzw==lti9bU3OVAYwF8Wk'
@@ -57,12 +57,54 @@ $('#submitBtn').on('click',function(){
         })
         .then(function (data) {
             console.log(data);
-        });          
+            printNutrition(data);
+          
+        });
 })
-       
+
+function printNutrition(data) {
+    let newRow = $('<tr>');
+
+    let itemInfo = $('<td>');
+    itemInfo.text(data[0].name);
+    newRow.append(itemInfo);
+
+    let calInfo = $('<td>');
+    calInfo.text(data[0].calories).addClass('calInfo');
+    newRow.append(calInfo);
+
+    let protInfo = $('<td>');
+    protInfo.text(data[0].protein_g).addClass('protInfo');
+    newRow.append(protInfo);
+
+    let carInfo = $('<td>');
+    carInfo.text(data[0].carbohydrates_total_g).addClass('carInfo');
+    newRow.append(carInfo);
+
+    let fatInfo = $('<td>');
+    fatInfo.text(data[0].fat_total_g).addClass('fatInfo');
+    newRow.append(fatInfo);
+
+    let sodInfo = $('<td>');
+    sodInfo.text(data[0].sodium_mg).addClass('sodInfo is-hidden-mobile');
+    newRow.append(sodInfo);
+
+    let cholInfo = $('<td>');
+    cholInfo.text(data[0].cholesterol_mg).addClass('cholInfo is-hidden-mobile');
+    newRow.append(cholInfo);
+
+    let remove = $('<td>');
+    let removeBtn = $('<button>')
+    removeBtn.addClass('removeBtn').text('X');
+    remove.append(removeBtn);
+    newRow.append(remove);
+
+    table.append(newRow);
+}
 
 
 
 
 
-        
+
+
