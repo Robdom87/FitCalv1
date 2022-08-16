79982861d3c128df1242d5e1cf2dd6c2e9453f42
CHRIS <chris@CHRISs-MBP.attlocal.net>
2022-08-15 20:07:48 -0700

    added modal and javascript functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,39 @@
 document.write('Hello!');
 
-//exercise button functionality (need button name to call function)
-// var exerciseBtn = document.getElementById('excerBtn')
 
-// exerciseBtn.addEventListener('click',function() {
-//     var input = document.createElement("input");
-//     input.type = "search";
-//     input.id = "searchBar";
-
-//     var searchBtn = document.createElement("button");
-//     btn.innerHTML = "Submit";
-//     btn.type = "submit";
-//     btn.name = "searchBtn";
-//     document.body.appendChild(input);
-// });
 $(function(){
     $(".exercise").hide();
 });
 
 $("#exerBtn").click(function() {
-    console.log("exercise"); // or alert($(this).attr('id'));
+    // console.log("exercise"); // or alert($(this).attr('id'));
     $('.nutrition').hide();
-
     $('.exercise').show();
-
-
 });
 
+$("#exerciseSearch").click(function() {
+    $('.modal').show();
+});
 
-// searchBtn.addEventListener('click', function() {
-//     var exerciseType = document.getElementById('searchBar').value;
-//     if (exerciseType = "") {
+$('.delete').click(function(){
+    $(".modal").hide();
+});
 
-//         $.ajax({
-//             method: 'GET',
-//             url: 'https://api.api-ninjas.com/v1/exercises?name=' + exerciseType,
-//             headers: { 'X-Api-Key': 'GKg0l9hlc0fRJEHUdIsVzw==lti9bU3OVAYwF8Wk'},
-//             contentType: 'application/json',
-//             success: function(result) {
-//                 console.log(result);
-//             },
-//             error: function ajaxError(jqXHR) {
-//                 console.error('Error: ', jqXHR.responseText);
-//             }
-//         });
+$('.cancelButton').click(function(){
+    $(".modal").hide();
+});
 
-//     }  
-// });
+let requestUrl = 'https://api.api-ninjas.com/v1/exercises?muscle&difficulty='
+fetch(requestUrl, {
+    headers: {
+        'X-Api-Key': 'GKg0l9hlc0fRJEHUdIsVzw==lti9bU3OVAYwF8Wk'
+    }
+})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+
 
